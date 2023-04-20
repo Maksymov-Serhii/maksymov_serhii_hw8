e720f13cc58ba82c185f9de043720e8473574707
Maksymov-Serhii <maksymov.serhii.10.01@gmail.com>
2023-04-20 23:51:38 +0300

    removed faulty elements

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,31 +1,9 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Clock.module.css"
-
-const Counter = ({ getCount }) => {
-  const [state, setState] = useState(null);
-
-  useEffect(() => {
-    const counter = getCount();
-    setState(counter);
-  }, [getCount]);
-
-  return (
-    <div className={styles.container}>
-      <h3>Counter</h3>
-      <p>{state}</p>
-    </div>
-  )
-}
 
 const Clock = () => {
 
   const [date, setDate] = useState(new Date());
-
-  // useMemo
-
-  const formattedTime = useMemo(() => {
-    return date.toLocaleTimeString();
-  }, [date]);
   
   const tick = () => {
     setDate(new Date());
@@ -38,13 +16,13 @@ const Clock = () => {
       () => tick(),
       1000
     );
-      
+
     // componentWillUnmount
     return () => {
       clearInterval(timerID);
     };
-
-  }, []);  
+    
+  }, []);
   
   const [prevDate, setPrevDate] = useState(null);
   
@@ -56,23 +34,10 @@ const Clock = () => {
     setPrevDate(date);
   }, [date, prevDate]);
 
-  // useCallback
-
-  const [count, setCount] = useState(-1);
-
-  const getCount = useCallback(() => {
-    return count;
-  }, [count]);
-  
-  useEffect(() => {
-    setCount(count => count + 1);
-  }, [date]);
-
   return (
     <div className={styles.container}>      
       <h1>Clock component</h1>
-      <h2>It is {formattedTime}.</h2>
-      <Counter getCount={getCount}/>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
     </div>
   )
 }
